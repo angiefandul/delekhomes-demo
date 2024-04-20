e90349c09acaece746006ed779ab3a8a905448a2
angiefandul <152406516+angiefandul@users.noreply.github.com>
2024-04-20 16:36:01 -0600

    adding registration test

--- cypress/e2e/1-getting-started/registrationTest.js
+++ cypress/e2e/1-getting-started/registrationTest.js
@@ -3,7 +3,20 @@
 describe('Registration', () => {
     it('Register new user', () => {
       
-        cy.get("https://dev.delekhomes.com/")
+        cy.visit("https://dev.delekhomes.com/");
+        cy.contains('Register').click();
+
+        //Filling out the form:
+        cy.get('input[name="firstName"]').type('Anzhela');
+        cy.get('input[name="lastName"').type('Fandul');
+        cy.get('input[name="email"]').type('anzhelaf@yahoo.com')
+        cy.get('input[name="password"]').type('12345678')
+
+        //check checkbox:
+        cy.get('input[type="checkbox"]').check().should('be.checked')
+        cy.get('input[type="checkbox"]').uncheck().should('not.be.checked')
         
+        cy.get('button[type="submit"]').click();
+
     })
   })
